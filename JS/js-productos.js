5eb1d824daa926437afab48b45bf965b44b81d6f
Lucas Mercado <lucasmmercado99@gmail.com>
2023-07-17 18:53:42 -0300

    Busqueda de productos

--- JS/js-productos.js
+++ JS/js-productos.js
@@ -1,7 +1,7 @@
 let carritoProductos = [];
 
-class Producto{
-    constructor(id, nombre, precio, descripcion, img){
+class Producto {
+    constructor(id, nombre, precio, descripcion, img) {
         this.id = id;
         this.nombre = nombre;
         this.precio = precio;
@@ -11,7 +11,7 @@ class Producto{
 }
 
 const productos = [
-    new Producto(1, "Arena" , 600, "Precio x metro cubico", "../img/ladrillo.png" ),
+    new Producto(1, "Arena", 600, "Precio x metro cubico", "../img/ladrillo.png"),
     new Producto(2, "Cal", 800, "Precio x bolsa", "../img/ladrillo.png"),
     new Producto(3, "Cemento", 950, "Precio x bolsa", "../img/ladrillo.png"),
     new Producto(4, "Pegamento Baldosas", 850, "Precio x bolsa", "../img/ladrillo.png"),
@@ -21,31 +21,36 @@ const productos = [
 
 const contenedorProductos = document.querySelector("#contenedor-productos");
 
-productos.forEach( producto => {
-    const artProductos = document.createElement("article");
-    artProductos.innerHTML = `
-        <article class="card-producto"> 
-            <img src=${producto.img}>
-            <div class="card-producto-body">
-                <hr>
-                <strong class="card-producto-price">${producto.precio}</strong>
-                <h3 class="card-producto-title">${producto.nombre}</h3>          
-                <button class="btn" id="btn-agregar-productos">Agregar producto</button>
-            </div>
-        </article>
-    `;
-    contenedorProductos.appendChild(artProductos);
+function mostrarProductos(productos) {
+    contenedorProductos.innerHTML = " ";
+    productos.forEach(producto => {
+        const artProductos = document.createElement("article");
+        artProductos.innerHTML = `
+            <article class="card-producto"> 
+                <img src=${producto.img}>
+                <div class="card-producto-body">
+                    <hr>
+                    <strong class="card-producto-price">${producto.precio}</strong>
+                    <h3 class="card-producto-title">${producto.nombre}</h3>          
+                    <button class="btn" id="btn-agregar-productos">Agregar producto</button>
+                </div>
+            </article>
+        `;
+        contenedorProductos.appendChild(artProductos);
 
-    const btnAgregar = artProductos.querySelector("#btn-agregar-productos");
+        const btnAgregar = artProductos.querySelector("#btn-agregar-productos");
 
-    btnAgregar.addEventListener("click", () => {
-        carritoProductos.push(producto);
+        btnAgregar.addEventListener("click", () => {
+            carritoProductos.push(producto);
 
-        actualizarCarrito(carritoProductos);
-        mostrarCarrito();
+            actualizarCarrito(carritoProductos);
+            mostrarCarrito();
+        })
+
     })
 
-})
+}
+
 
 function actualizarCarrito(carrito) {
     localStorage.setItem("carrito", JSON.stringify(carrito));
@@ -58,7 +63,7 @@ function mostrarCarrito() {
     const carritoGuardado = localStorage.getItem("carrito");
     let precioTotal = 0;
 
-    if(carritoGuardado) {
+    if (carritoGuardado) {
         const carritoParse = JSON.parse(carritoGuardado);
         carritoParse.forEach(producto => {
             const artCarrito = document.createElement("article");
@@ -84,3 +89,18 @@ function mostrarCarrito() {
     }
 
 }
+
+function buscarProducto() {
+    let productoIngresado = document.getElementById("producto-buscar").value;
+    let productosFiltrados = productos.filter(producto => producto.nombre.includes(productoIngresado));
+
+    if (productosFiltrados.length != 0 ) {
+        mostrarProductos(productosFiltrados);
+    }else {
+        contenedorProductos.innerHTML = "No se encontraron productos con ese nombre.";
+    }
+
+
+}
+
+mostrarProductos(productos);
